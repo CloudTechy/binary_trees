f2539bf0a7e1add32a7f089bf70fbf1ffce303b4
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-03-28 12:03:03 -0700

    should work

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,5 +10,14 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	return NULL;
+	binary_tree_t *child_node;
+
+	child_node = malloc(sizeof(binary_tree_t));
+	if (child_node == NULL)
+		return (NULL);
+	child_node->n = value;
+	child_node->parent = parent;
+	child_node->left = NULL;
+	child_node->right = NULL;
+	return (child_node);
 }
