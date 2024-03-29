51d48009260b47423f2eb642b2b63338d67cc51f
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-03-29 03:31:29 -0700

    done

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,8 +12,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	binary_tree_t *aux;
-	
+
 	if (parent == NULL)
 		return (NULL);
 	new_node = binary_tree_node(parent, value);
@@ -25,7 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
-	
+
 	parent->left = new_node;
 
 }
