add0d96d1f30e1557080a6dbbf91cf8195cd9893
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-03-29 05:02:20 -0700

    done

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - gets the height of a binary tree
+ * @tree: binary tree
+ * Return: size_t int
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0, r = 0;
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0)
+}
