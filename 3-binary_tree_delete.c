be401510bad160a9960390e4df1874aa9c354122
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-03-29 04:09:14 -0700

    fixed

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,4 +1,4 @@
-#import "binary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_delete - deletes the entire tree
@@ -9,7 +9,7 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree != NULL)
 	{
 		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right)
+		binary_tree_delete(tree->right);
 		free(tree);
 	}
 }
